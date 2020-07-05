7dcedead76df3b3f6fd0205e4aa10e2d924cbfc2
zzzzz1st <zzzzz1st@yahoo.com>
2020-07-05 11:42:57 +0200

    this is the version 1.03 (windows icon added)

--- Calculator.pro
+++ Calculator.pro
@@ -32,3 +32,5 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 RESOURCES += \
     recources.qrc
+
+RC_FILE = iconres.rc
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon(":/images/calculator_mainIcon.png"));
+    a.setWindowIcon(QIcon(":/images/calculator_main.ico"));
     Calculator w;
     w.show();
     return a.exec();
